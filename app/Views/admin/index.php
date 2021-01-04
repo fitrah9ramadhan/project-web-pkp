fdcdce0cf1365f11fcd076ee78987c14ae32c7f2
fitrah9ramadhan <fitrah9ramadhan@gmail.com>
2021-01-04 17:15:04 +0700

    tampilan index admin website

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -1,22 +1,12 @@
-<?php echo $this->extend('layout_dua/template'); ?>
+<?= $this->extend('layout_admin/template') ?>
 
-<?php echo $this->section('content'); ?>
+<?= $this->section('content'); ?>
 
 <div class="header-halaman"></div>
-<div class="container-fluid">
-	<a class= "btn btn-danger" href="/adminlogin/logout">Logout</a>
-	<h1>Options</h1>
-	<a href="/admin/crud_berita">CRUD Berita</a>
-	<a href="/admin/crud_pemdes">CRUD Pemdes</a>
-	<a href="/admin/edit_kontak">Edit Kontak</a>
-	<a href="/admin/edit_profil">Edit Profil</a>
-	<a href="/admin/tambah_agenda">Tambah Agenda</a>
-	<a href="/admin/edit_camat">Edit Camat</a>
-</div>
+<h1>Selamat Datang Admin</h1>
 
-<a href="/">Kembali</a>
 
-<?php echo $this->endSection(); ?>'
+<?= $this->endSection(); ?>
 
 
 
